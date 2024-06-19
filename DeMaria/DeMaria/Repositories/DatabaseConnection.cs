9b8eef6cac866e0dee62aff9f80bd14c0ae27e5f
João Pedro <jaopedro_santos@hotmail.com>
2024-06-19 03:48:03 -0300

    Criadas pastas do projeto e classe para conexão com o banco.

---
+++ DeMaria/DeMaria/Repositories/DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DeMaria.Repositories
+{
+    public class DatabaseConnection
+    {
+        private string connectionString = "";
+        private NpgsqlConnection connection;
+
+        public NpgsqlConnection Connection
+        {
+            get
+            {
+                if (connection == null)
+                {
+                    connection = new NpgsqlConnection(connectionString);
+                }
+                return connection;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+                connection.Dispose();
+                connection = null;
+            }
+        }
+    }
+}
